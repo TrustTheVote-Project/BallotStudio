150a55fe3c7f9f8a3dd52f9193c50500f939847b
Brian Olson <brian.olson@gmail.com>
2020-03-15 22:38:41 -0400

    Contest object. cleanup.

--- a.py
+++ a.py
@@ -20,14 +20,6 @@ def bubble(c, width=12*mm, height=4*mm, r=1*mm):
     pth.roundRect(0, 0, width, height, r)
     return pth
 
-#mainfontname = 'Times-Roman'
-#mainfontname = 'Liberation-Serif'
-#fontpath = '/usr/share/fonts/truetype/liberation/LiberationSerif-Regular.ttf'
-mainfontname = 'Liberation Sans'
-#fontpath = '/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf'
-#fontpathbold = '/usr/share/fonts/truetype/liberation/LiberationSans-Bold.ttf'
-boldfontname = 'Liberation Sans Bold'
-
 
 class Bfont:
     def __init__(self, path, name=None):
@@ -54,16 +46,6 @@ class Bfont:
                     self.name = xn.toUnicode()
                     break
 
-# ftt = fontTools.ttLib.TTFont(fontpath)
-# # do some font metrics about the size of the capital letters
-# caps = [chr(x) for x in range(ord('A'), ord('Z')+1)]
-# caps.remove('Q') # outlier descender
-# glyfminmax = [(ftt['glyf'][glyc].yMax, ftt['glyf'][glyc].yMin, glyc) for glyc in caps]
-# gmaxes = [x[0] for x in glyfminmax]
-# gmins = [x[1] for x in glyfminmax]
-# capmin = statistics.median(gmins)
-# capmax = statistics.median(gmaxes)
-# capHeightPerPt = (capmax - capmin) / 2048
 
 fonts = {}
 
@@ -73,16 +55,17 @@ for fpath in glob.glob('/usr/share/fonts/truetype/liberation/*.ttf'):
 
 print('fonts: ' + ', '.join([repr(n) for n in fonts.keys()]))
 
-# pointsize = 26
-# bf = fonts[mainfontname]
-# capHeight = bf.capHeightPerPt * pointsize
-# candsize = pointsize * 1.4
-# bubbleHeight = min(3*mm, capHeight)
-# bubbleYShim = (capHeight - bubbleHeight) / 2.0
-
 
 class Settings:
     def __init__(self):
+        self.titleFontName = 'Liberation Sans Bold'
+        self.titleFontSize = 12
+        self.titleBGColor = (.85, .85, .85)
+        self.titleLeading = self.titleFontSize * 1.4
+        self.subtitleFontName = 'Liberation Sans Bold'
+        self.subtitleFontSize = 12
+        self.subtitleBGColor = ()
+        self.subtitleLeading = self.subtitleFontSize * 1.4
         self.candidateFontName = 'Liberation Sans Bold'
         self.candidateFontSize = 12
         self.candidateLeading = 13
@@ -139,7 +122,7 @@ class Choice:
         c.setStrokeColorRGB(0,0,0)
         c.setLineWidth(0.25)
         sepy = ypos - (0.1 * inch)
-        c.line(textx, sepy, width - (gs.bubbleLeftPad + gs.bubbleWidth + gs.bubbleRightPad), sepy)
+        c.line(textx, sepy, x+width, sepy)
         return
     def sepLineBelow(self, c, x, y):
         c.setStrokeColorRGB(0,0,0)
@@ -152,43 +135,82 @@ class Choice:
         c.setLineWidth(0.5)
         c.setDash(None)
 
-#lfont = TTFont(mainfontname, fontpath)
-#pdfmetrics.registerFont(lfont)
+
+class Contest:
+    def __init__(self, name, title=None, subtitle=None, choices=None):
+        self.name = name
+        self.title = title or name
+        self.subtitle = subtitle
+        self.choices = choices
+        self._choices_height = None
+        self._height = None
+        self._maxChoiceHeight = None
+    def height(self):
+        if self._height is None:
+            choices = self.choices or []
+            self._maxChoiceHeight = max([x.height() for x in choices])
+            ch = self._maxChoiceHeight * len(choices)
+            ch += 4 # top and bottom border
+            ch += gs.titleLeading + gs.subtitleLeading
+            ch += 0.1 * inch # header-choice gap
+            ch += 0.1 * inch # bottom padding
+            self._height = ch
+        return self._height
+    def draw(self, c, x, y, width=(7.5/2)*inch - 1):
+        # x,y is a top,left
+        # top border
+        c.setStrokeColorRGB(0,0,0)
+        c.setLineWidth(3)
+        c.line(x, y, x + width, y)
+        # left border
+        c.setLineWidth(1)
+        c.line(x, y, x, y-self.height())
+        # bottom border
+        c.line(x, y-self.height(), x+width, y-self.height())
+
+        pos = y - 1.5
+        # title
+        c.setStrokeColorRGB(*gs.titleBGColor)
+        c.setFillColorRGB(*gs.titleBGColor)
+        c.rect(x + 1, pos - gs.titleLeading, width - 1, gs.titleLeading, fill=1)
+        c.setFillColorRGB(0,0,0)
+        c.setStrokeColorRGB(0,0,0)
+        txto = c.beginText(x + 1 + (0.1 * inch), pos - gs.titleFontSize)
+        txto.setFont(gs.titleFontName, gs.titleFontSize)
+        txto.textLines(self.title)
+        c.drawText(txto)
+        pos -= gs.titleLeading
+        # subtitle
+        c.setStrokeColorCMYK(.1,0,0,0)
+        c.setFillColorCMYK(.1,0,0,0)
+        c.rect(x + 1, pos - gs.subtitleLeading, width - 1, gs.subtitleLeading, fill=1)
+        c.setFillColorRGB(0,0,0)
+        c.setStrokeColorRGB(0,0,0)
+        txto = c.beginText(x + 1 + (0.1 * inch), pos - gs.subtitleFontSize)
+        txto.setFont(gs.subtitleFontName, gs.subtitleFontSize)
+        txto.textLines(self.subtitle)
+        c.drawText(txto)
+        pos -= gs.subtitleLeading
+        pos -= 0.1 * inch # header-choice gap
+        c.setFillColorRGB(0,0,0)
+        c.setStrokeColorRGB(0,0,0)
+        choices = self.choices or []
+        for ch in choices:
+            ch.draw(c, x + 1, pos, width=width - 1)
+            pos -= self._maxChoiceHeight
+        return
 
 
 c = canvas.Canvas('/tmp/a.pdf', pagesize=letter) # pageCompression=1
 widthpt, heightpt = letter
 
 nowstrFontSize = 12
+mainfontname = 'Liberation Sans'
 dtw = pdfmetrics.stringWidth(nowstr, mainfontname, nowstrFontSize)
 c.setFont(mainfontname, nowstrFontSize)
 c.drawString(widthpt - 0.5*inch - dtw, heightpt - 0.5*inch - nowstrFontSize*1.2, nowstr)
 c.setTitle('ballot test ' + nowstr)
 
-# def drawChoice(topliney, nameText):
-#     # TODO: candidate subtext (occupation, etc), multi-line capable
-#     # TODO: boxes, chartjunk, etc
-#     # TODO: multi-line nameText
-#     # TODO: RCV bubbles
-#     # bubble
-#     c.setStrokeColorRGB(0,0,0)
-#     c.setFillColorRGB(1,1,1)
-#     c.roundRect(0.5*inch, topliney + bubbleYShim, 12*mm, bubbleHeight, radius=bubbleHeight/2)
-
-#     if False:
-#         # debugging marks
-#         c.setStrokeColorRGB(1,1,1)
-#         c.setFillColorRGB(0,0.3,0)
-#         c.rect(0.5*inch + 13*mm, topliney, 1*mm, pointsize, stroke=0, fill=1)
-#         c.rect(0.5*inch + 14.1*mm, topliney, 1*mm, capHeight, stroke=0, fill=1)
-
-#     # candidate name
-#     c.setFont(boldfontname, pointsize)
-#     c.setFillColorRGB(0,0,0)
-#     txto = c.beginText(0.5*inch + 15*mm,topliney)
-#     txto.textLines(nameText)
-#     c.drawText(txto)
-#     return
 
 candidateNames = [
     'Alice Argyle',
@@ -208,16 +230,15 @@ choices = [
     Choice('Elaine Entwhistle', 'Electable Entertainers'),
 ]
 
-maxChoiceHeight = max([x.height() for x in choices])
+therace = Contest('Everything', 'The Race for Everything', 'Choose as many as you like', choices)
 
-pos = heightpt - 0.5*inch # - pointsize * 1.2
-for ch in choices:
-    ch.draw(c, 0.5*inch, pos)
-    pos -= maxChoiceHeight
-# for nameText in candidateNames:
-#     drawChoice(pos, nameText)
-#     pos -= candsize
+# maxChoiceHeight = max([x.height() for x in choices])
+# pos = heightpt - 0.5*inch # - pointsize * 1.2
+# for ch in choices:
+#     ch.draw(c, 0.5*inch, pos)
+#     pos -= maxChoiceHeight
 
+therace.draw(c, 0.5 * inch, heightpt - 0.5*inch)
 
 c.showPage()
 c.save()
