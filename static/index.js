988359493f0407ed414709cb2a37cc2ea7a05e55
Brian Olson <brian.olson@gmail.com>
2020-04-26 12:26:20 -0400

    editing starting to work

--- static/index.js
+++ static/index.js
@@ -23,19 +23,59 @@
 	return null;
     };
     var sequenceCounters = {};
+    var usedSeqIds = {};
+    var claimSeq = function(seqName, atid) {
+	var sv = usedSeqIds[seqName];
+	if (!sv) {
+	    sv = {};
+	    usedSeqIds[seqName] = sv;
+	}
+	sv[atid] = true;
+    };
+    var atidTaken = function(seqName, atid) {
+	var sv = usedSeqIds[seqName];
+	if (!sv) {
+	    return false;
+	}
+	return sv[atid];
+    }
     var seq = function(seqName) {
 	var sv = sequenceCounters[seqName];
-	if ((sv === undefined) || (sv == null)) {
+	if (!sv) {
 	    sv = 0;
 	}
-	sv++;
-	sequenceCounters[seqName] = sv;
-	return seqName + sv;
+	while (true) {
+	    sv++;
+	    sequenceCounters[seqName] = sv;
+	    var atid = seqName + sv;
+	    if (!atidTaken(seqName, atid)) {
+		claimSeq(seqName, atid);
+		return atid;
+	    }
+	}
     };
+    var tmplForAttype = {
+	"ElectionResults.Party":{"tmpl":"partytmpl", "seq":"party"},
+	"ElectionResults.Person":{"tmpl":"persontmpl", "seq":"pers"},
+	"ElectionResults.Office":{"tmpl":"officetmpl", "seq":"office"},
+    };
+    // Used in loading from stored spec
+    var newForAtType = function(attype) {
+	var ti = tmplForAttype[attype];
+	if (!ti) {
+	    return null;
+	}
+	var tmpl = document.getElementById(ti.tmpl);
+	var pti = document.importNode(tmpl.content, true);
+	// var atidelem = firstChildOfClass(pti, "atid");
+	// var atid = seq(ti.seq);
+	// atidelem.value = atid;
+	return pti;
+    };
+    // Used when a [new thing] button is pressed by a user
     var instantiate = function(tmplId, sectionId, seqName) {
 	var tmpl = document.getElementById(tmplId);
 	var pti = document.importNode(tmpl.content, true);
-	//var atidelem = pti.getElementsByClassName("atid")[0];
 	var atidelem = firstChildOfClass(pti, "atid");
 	var atid = seq(seqName);
 	atidelem.setAttribute("data-atid", atid);
@@ -43,30 +83,24 @@
 	var section = document.getElementById(sectionId);
 	section.appendChild(pti);
     };
-    var newParty = function() {
-	instantiate("partytmpl", "parties", "party");
-	// var partytmpl = document.getElementById("partytmpl");
-	// var pti = document.importNode(partytmpl.content, true);
-	// var parties = document.getElementById("parties");
-	// parties.appendChild(pti);
-    };
-    document.getElementById("newparty").onclick = newParty;
+    document.getElementById("newparty").onclick = function(){instantiate("partytmpl", "parties", "party");};
     document.getElementById("newperson").onclick = function(){instantiate("persontmpl", "people", "pers");};
+    document.getElementById("newoffice").onclick = function(){instantiate("officetmpl", "offices", "office");};
 
-    var gatherJson = function(elem) {
-	if (hasClass(elem.children[i], "arraygroup")) {
-	    var ob = [];
-	    for (var i = 0; elem.children[i]; i++) {
-		var to = gatherJson(elem.children[i]);
-		if (to === undefined || to == null) {
-		    continue;
-		}
-		ob.push(to)
+    var gatherArray = function(elem) {
+	var ob = [];
+	for (var i = 0; elem.children[i]; i++) {
+	    var to = gatherJson(elem.children[i]);
+	    if (to === undefined || to == null) {
+		continue;
 	    }
-	    return ob;
+	    ob.push(to)
 	}
+	return ob;
+    }
+    var gatherJson = function(elem) {
 	var ob = {};
-	var oblist = [];
+	var any = false
 	for (var i = 0, te; te = elem.children[i]; i++) {
 	    if (te.tagName == "INPUT") {
 		var fieldname = te.className;
@@ -75,8 +109,100 @@
 		} else if (fieldname == "atid") {
 		    fieldname = "@id";
 		}
-		ob[fieldname] = te.valueOf();
+		ob[fieldname] = te.value;
+		any = true;
+	    } else if (hasClass(te, "arraygroup")) {
+		var fieldname = te.getAttribute("data-name");
+		ob[fieldname] = gatherArray(te);
+		any = true;
+	    } else {
+		var to = gatherJson(te);
+		if (to != null) {
+		    for (var k in to) {
+			ob[k]=to[k];
+			any = true;
+		    }
+		}
 	    }
 	}
+	if (any) {
+	    //console.log("gathered ", ob);
+	    return ob;
+	} else {
+	    return null;
+	}
     };
+
+    var isEmpty = function(ob) {
+	for (var k in ob) {
+	    return false;
+	}
+	return true;
+    };
+
+    var pushOb = function(elem, ob){
+	if (!elem.children) {
+	    console.log("pushOb cannot descend ", elem)
+	    return false;
+	}
+	for (var i = 0, te; te = elem.children[i]; i++) {
+	    if (hasClass(te, "arraygroup")) {
+		var fieldname = te.getAttribute("data-name");
+		var av = ob[fieldname];
+		if (av === undefined || av == null || (!av.length)) {
+		    console.log("no data for ", av);
+		    continue;
+		}
+		for (var j = 0, jv; jv = av[j]; j++) {
+		    var je = te.children[j];
+		    if (je === undefined) {
+			var attype = jv["@type"];
+			if (attype) {
+			    je = newForAtType(attype);
+			    te.appendChild(je);
+			    je = te.children[j];
+			} else {
+			    console.log("don't know how to add value ", jv)
+			    continue;
+			}
+		    }
+		    pushOb(je, jv);
+		}
+		delete ob[fieldname];
+		if (isEmpty(ob)) {
+		    return true;
+		}
+	    } else if (te.tagName == "INPUT") {
+		var fieldname = te.className;
+		if (fieldname == "attype") {
+		    fieldname = "@type";
+		} else if (fieldname == "atid") {
+		    fieldname = "@id";
+		}
+		var ov = ob[fieldname];
+		if (ov) {
+		    te.value = ov;
+		    delete ob[fieldname];
+		    if (isEmpty(ob)) {
+			return true;
+		    }
+		}
+	    } else {
+		var consumed = pushOb(te, ob);
+		if (isEmpty(ob)) {
+		    return true;
+		}
+	    }
+	}
+	return false;
+    };
+
+    var debugbutton = document.getElementById("debugbutton");
+    debugbutton.onclick = function() {
+	var js = gatherJson(document.body);
+	document.getElementById("debugdiv").innerHTML = JSON.stringify(js);
+    };
+
+    var savedObj = {"Party":[{"@id":"party1","@type":"ElectionResults.Party","Name":"Stupid","Abbreviation":"","Color":"","IsRecognizedParty":"on","LogoUri":"","Slogan":""},{"@id":"party2","@type":"ElectionResults.Party","Name":"Evil","Abbreviation":"","Color":"","IsRecognizedParty":"on","LogoUri":"","Slogan":""}],"Person":[{"@id":"person1","@type":"ElectionResults.Person","FullName":"SOMEGUY","Prefix":"","FirstName":"","MiddleName":"","LastName":"","Suffix":"","Nickname":"","Title":"","Profession":"","DateOfBirth":""}],"Office":[]};
+    pushOb(document.body, savedObj);
 })();
